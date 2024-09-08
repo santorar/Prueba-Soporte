d1ced2f8ad39067d15ee5e8ca16602136625694c
santorar <estractor179@gmail.com>
2024-09-07 19:12:25 -0500

    Creados métodos fetchTasks y completeTask y actualizados los métodos anteriores para devolver la información en vez de una redirection

--- resources/js/store.js
+++ resources/js/store.js
@@ -9,8 +9,22 @@ export default new Vuex.Store({
         tasks: [] // Estado inicial para las tareas
     },
     mutations: {
-        ADD_TASK(state, task) {
-            state.tasks.push(task);
+        FETCH_TASKS(state, tasks){
+            state.tasks = tasks;
+        },
+        ADD_TASK(state, {task, filter}) {
+            if(filter !== "completed")
+                state.tasks.push(task);
+        },
+        COMPLETE_TASK(state, {completedTask, filter}){
+            if(filter !== "pending"){
+                const index = state.tasks.findIndex(t => t.id === completedTask.id);
+                if (index !== -1){
+                    Vue.set(state.tasks, index, completedTask)
+                }
+            }else{
+                state.tasks = state.tasks.filter(t => t.id !== completedTask.id);
+            }
         },
         UPDATE_TASK(state, updatedTask) {
             const index = state.tasks.findIndex(t => t.id === updatedTask.id);
@@ -23,13 +37,32 @@ export default new Vuex.Store({
         }
     },
     actions: {
-        addTask({ commit }, task) {
+        fetchTasks({ commit }, payload){
+            axios.get(`/tasks`,payload)
+                .then(response => {
+                    commit('FETCH_TASKS', response.data)
+                })
+                .catch(error => {
+                    console.error("Error getting tasks:", error);
+                });
+        },
+        addTask({ commit }, {task, filter}) {
             axios.post('/tasks', task)
                 .then(response => {
-                    commit('ADD_TASK', response.data);
+                    task = response.data;
+                    commit('ADD_TASK', {task, filter});
                 })
                 .catch(error => {
                     console.error("Error adding task:", error);
+                });
+        },
+        completeTask({ commit }, {taskId, filter}){
+            axios.post(`/tasks-complete/${taskId}`)
+                .then(response => {
+                    commit('COMPLETE_TASK',{completedTask: response.data, filter: filter});
+                })
+                .catch(error => {
+                    console.error("Error completing task:", error);
                 });
         },
         updateTask({ commit }, task) {
