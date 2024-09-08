798328d6de968e13e54fb7f895f9492045355f2e
santorar <estractor179@gmail.com>
2024-09-07 19:51:10 -0500

    cambio de URL a la cual se hacen las consultas de 127.0.0.1 a localhost para evitar errores de CSRF y cambio de nombre de archivo task.php a task.blade.php para que tome correctamente los parámetros por blade

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 Vue.use(Vuex);
 // Configuración global de Axios
-axios.defaults.baseURL = 'http://127.0.0.1:8000'; // Cambia esto a tu URL base si es necesario
+axios.defaults.baseURL = 'http://localhost:8000'; // Cambia esto a tu URL base si es necesario
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 const app = new Vue({
     el: '#app',
