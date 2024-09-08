f491634d5565e275d26a84ab0aa2a8a2ecc8ef7c
santorar <estractor179@gmail.com>
2024-09-07 20:21:42 -0500

    Agregado un select para el filtrado de las tareas dependiendo de su estado, agregada flag para la desactivación del botón Complete, agregado spinner de cargado al renderizar el listado de tareas y agregada acción para refrescar las tareas.

--- resources/js/components/TaskList.vue
+++ resources/js/components/TaskList.vue
@@ -1,7 +1,18 @@
 <template>
     <div class="container mt-5">
         <h1 class="text-center mb-4">Task List</h1>
-        <ul class="list-group mb-4">
+        <div class="form-group">
+            <label class="mb-1 mr-2" for="selectFilter">Filter</label>
+            <select class="custom-select my-1 mr-sm-2" v-model="selectedFilter" id="selectFilter">
+                <option v-for="option in optionFilters" :value="option.value">
+                    {{ option.text }}
+                </option>
+            </select>
+        </div>
+        <div v-show="loading" class="spinner-border spinner-border-sm" role="status">
+          <span class="sr-only">Loading...</span>
+        </div>
+        <ul class="list-group mb-4" v-show="!loading">
             <li v-for="task in tasks" :key="task.id" class="list-group-item d-flex justify-content-between align-items-center">
                 <div>
                     <h5 class="mb-1">{{ task.title }}</h5>
@@ -9,7 +20,7 @@
                     <small class="text-muted">Assigned to: {{ task.user }}</small>
                 </div>
                 <div>
-                    <button class="btn btn-success btn-sm mr-2" @click="completeTask(task.id)">Complete</button>
+                    <button :disabled=!!task.completed class="btn btn-success btn-sm mr-2" @click="completeTask(task.id)">Complete</button>
                     <button class="btn btn-danger btn-sm" @click="deleteTask(task.id)">Delete</button>
                 </div>
             </li>
@@ -39,11 +50,18 @@ export default {
                 title: '',
                 description: '',
                 user: ''
-            }
+            },
+            selectedFilter: null,
+            loading: false,
+            optionFilters: [
+                { value: null, text: "All"},
+                { value: "pending", text: "Pending"},
+                { value: "completed", text: "Completed"}
+            ]
         };
     },
     computed: {
-        ...mapState(['tasks']) // Simplificado para mapState
+        ...mapState(['tasks']), // Simplificado para mapState
     },
     methods: {
         ...mapActions(['fetchTasks', 'addTask', 'completeTask', 'deleteTask']),
@@ -52,9 +70,8 @@ export default {
                 alert('Both title and description are required');
                 return;
             }
-
             // Se utiliza la acción 'addTask' y luego se limpia el formulario
-            this.$store.dispatch('addTask', this.newTask).then(() => {
+            this.$store.dispatch('addTask', {task: this.newTask, filter: this.selectedFilter}).then(() => {
                 this.newTask.title = '';
                 this.newTask.description = '';
                 this.newTask.user = '';
@@ -64,7 +81,8 @@ export default {
         },
         completeTask(taskId) {
             // Se utiliza la acción 'completeTask'
-            this.$store.dispatch('completeTask', taskId).catch(error => {
+            this.$store.dispatch('completeTask', {taskId: taskId, filter: this.selectedFilter}).then(()=>{
+            }).catch(error => {
                 console.error('Error completing task:', error);
             });
         },
@@ -73,10 +91,25 @@ export default {
             this.$store.dispatch('deleteTask', taskId).catch(error => {
                 console.error('Error deleting task:', error);
             });
+        },
+        refreshTasks() {
+            // Actualiza las tareas usando la acción 'fetchTasks'
+            this.loading = true;
+            this.$store.dispatch('fetchTasks',{params:{ filter: this.selectedFilter}}).catch(error => {
+                console.error("Error refreshing tasks:", error);
+            });
+            this.loading = false;
+        }
+    },
+    watch: {
+        // Actualiza las tareas cuando se cambia el filtro
+        selectedFilter() {
+            this.refreshTasks();
         }
     },
     mounted() {
-
+        // Actualiza las tareas al montar el componente
+        this.refreshTasks();
     }
 };
 </script>
